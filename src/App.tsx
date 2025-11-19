de0b244f2487d5e03486092b1be6d4c10d209f47
gautamwecodepros <gautamwecodepros@gmail.com>
2025-11-19 14:47:23 +0530

    Hero section progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-
-import './App.css'
+import "./App.css";
+import HeroSection from "./components/hero-section";
 
 function App() {
-
-
- 
+  return <HeroSection />;
 }
 
-export default App
+export default App;
---
+++ src/components/hero-section.tsx
@@ -0,0 +1,13 @@
+import "./hero-section.css";
+
+const HeroSection = () => {
+  return (
+    <section className="hero-section">
+      <div className="hero-text">
+        <p className="hero-text-btn1">Brand Physics & IP Licensing</p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
